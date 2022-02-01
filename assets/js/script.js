16193561b8f6731dc9e5204ad44e262b79658d52
Expired <imoses2@hotmail.com>
2022-01-31 19:22:33 -0700

    fixes #4 - gets info from wikidata

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 //variables
 //==================================
 var id = "Q79015";	//superman->this will update with form update
+var title = "superman";
+// var title = "Katniss Everdeen";
 var	idArr = [];
-var apiUrlBase = `https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&origin=*&languages=en&ids=`
-
+var apiUrlBase = `https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&origin=*&languages=en&normalize=yes`
+var idBase = '&ids=';
+var titleBase = '&sites=enwiki&titles=';
 
 
 
@@ -12,8 +15,29 @@ var apiUrlBase = `https://www.wikidata.org/w/api.php?action=wbgetentities&format
 //functions
 //====================================
 //step 1: get superman data from wikidata
+function fetchTitle(title) {
+	fetch(apiUrlBase+titleBase+title)
+	.then(function(response) {
+		if(!response.ok) {
+			console.log("response bad");
+			return false;
+		}
+		response.json()
+		.then(function(data) {
+			console.log(data);
+			for (id in data.entities) {
+				// console.log(id);
+				displayId(data);
+			}
+			
+			return true;
+		});
+	});
+}
+
+//alternative step 1: get Q79015 (superman) data from wikidata
 function fetchId(id) {
-	fetch(apiUrlBase+id)
+	fetch(apiUrlBase+idBase+id)
 	.then(function(response) {
 		if(!response.ok) {
 			console.log("response bad");
@@ -38,13 +62,13 @@ function displayId(data) {
 		// console.log(claim[i].mainsnak.datavalue.value.id);
 		idArr.push(claim[i].mainsnak.datavalue.value.id);
 	}
-	id = idArr.join("%7C");
+	id = idArr.join("|");
 	fetchCreators(id);
 }
 
 //step 3: get the people superman was created by
 function fetchCreators(id){
-	fetch(apiUrlBase+id)
+	fetch(apiUrlBase+idBase+id)
 	.then(function(response) {
 		if(!response.ok) {
 			console.log("response bad");
@@ -64,9 +88,14 @@ function displayCreators(data) {
 	for(i=0; i<idArr.length; i++) {
 		var item = data.entities[idArr[i]];
 		console.log(item.labels.en.value);
-		console.log("who died on");
 		var claim = item.claims.P570;
-		console.log(claim[0].mainsnak.datavalue.value.time);
+		if(claim) {
+			console.log("who died on");
+			console.log(claim[0].mainsnak.datavalue.value.time);
+		}
+		else {	//still alive, or data is incomplete
+			console.log("who is still alive")
+		}
 	}
 }
 
@@ -89,7 +118,8 @@ function displayCreators(data) {
 
 //body
 //=====================================
-fetchId(id);
+// fetchId(id);
+fetchTitle(title);
 
 
 
